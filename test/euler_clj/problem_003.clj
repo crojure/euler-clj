8ea23f96b6f12cfbbaf38e6b9462fa96d0252880
twcrone <twcrone@gmail.com>
2016-11-25 11:53:06 -0500

    Add solution for problem 3 that performs more betterer

---
+++ test/euler_clj/problem_003.clj
@@ -0,0 +1,44 @@
+(ns euler-clj.problem-003
+  (:require [clojure.test :refer :all]))
+
+; Project Euler
+;
+; Largest Prime Factor
+;
+; Problem 3
+;
+; The prime factors of 13195 are 5, 7, 13 and 29.
+;
+; What is the largest prime factor of the number 600851475143 ?
+;
+; Solution by twcrone
+
+(defn is-factor-of? [base num]
+  (zero? (mod base num)))
+
+(defn prime? [n]
+  (not-any? (partial is-factor-of? n) (range 2 (long (Math/ceil (Math/sqrt n))))))
+
+(defn candidates [n]
+  (range (long (Math/floor (Math/sqrt n))) 2 -1))
+
+(defn prime-factors-of [n]
+  (filter prime? (filter (partial is-factor-of? n) (candidates n))))
+
+(defn solve-for [n]
+  (first (prime-factors-of 600851475143)))
+
+; test
+(deftest is-factor-of-true
+  (is (is-factor-of? 10 2)))
+
+(deftest is-factor-of-false
+  (is (not (is-factor-of? 10 3))))
+
+(deftest base-case
+  (is (solve-for 13195) 29))
+
+(run-tests 'euler-clj.problem-003)
+
+; main
+(println "Problem 3 Solution: " (solve-for 600851475143))
